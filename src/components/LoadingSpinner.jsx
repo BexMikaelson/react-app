ae1f38ace620181dbefe5ff6182f54620eab256d
Heidi sjöberg <heidi.alexx8@gmail.com>
2022-09-29 12:27:49 +0200

    creating my loading spinner

---
+++ src/components/LoadingSpinner.jsx
@@ -0,0 +1,9 @@
+const LoadingSpinner = () => {
+    return (
+    <div className="spinner-container">
+        <div className="loading-spinner-center"></div>
+    </div>
+    )
+}
+
+export default LoadingSpinner
